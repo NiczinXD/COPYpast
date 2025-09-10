00d2bf79c732d7206fe88b46296ab92715ff23c4
NiczinXD <nicolasxpinheiro@gmail.com>
2025-09-10 16:07:10 -0300

    Novas implementações + Easter Eggs

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
             li.textContent = item;
             lista.appendChild(li);
 
+            
             const copyButton = document.createElement('button');
             copyButton.textContent = 'Copy';
             copyButton.classList.add('copy_button');
@@ -35,6 +36,51 @@ document.addEventListener('DOMContentLoaded', () => {
             removeButton.classList.add('remove_button');
             removeButton.addEventListener('click', () => removeCopy(item));
             li.appendChild(removeButton);
+            
+            if(item === 'voceNãoVaiAcharIsso111'){
+                const easterEggPageLink = document.createElement('a');
+                easterEggPageLink.href = 'easter.html';
+                easterEggPageLink.textContent = 'Easter Egg!';
+                li.appendChild(easterEggPageLink);
+                li.removeChild(copyButton);
+                li.appendChild(removeButton);
+            }
+
+            if(item === 'bazinga!'){
+                const img = document.getElementById('bazinga!!!');
+                img.hidden = false;
+
+                setTimeout(() => {
+                    img.hidden = true;
+                }, 8000);
+            }
+
+            if(item === '18/07/2012'){
+                const img = document.getElementById('18/07/2012');
+                img.hidden = false;
+
+                setTimeout(() => {
+                    img.hidden = true;
+                }, 8000);
+            }
+
+            if(item === 'Jurassic Park'){
+                const img = document.getElementById('JP3');
+                img.hidden = false;
+
+                setTimeout(() => {
+                    img.hidden = true;
+                }, 8000);
+            }
+
+            if(item === 'corinthians vs flamengo'){
+                const img = document.getElementById('corinthians_vs_flamengo');
+                img.hidden = false;
+
+                setTimeout(() => {
+                    img.hidden = true;
+                }, 8000);
+            }
         });
     }
 
